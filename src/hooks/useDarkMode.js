5a4c2b5e20d51af6e8946ba41cf49d576077aad7
Andrew Ruiz <nukedoom@gmail.com>
2020-10-06 12:48:24 -0700

    Final Project

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+
+// Might need to change this to {useLocalStorage} depending on how we originally exported. Have to test and see.
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = (initialValue) => {
+    const [someValue, setSomeValue] = useLocalStorage('Any key works here', initialValue)
+
+    return [someValue, setSomeValue]
+
+}
+
+export default useDarkMode;
